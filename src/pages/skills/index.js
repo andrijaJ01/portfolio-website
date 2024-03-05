15051c3521ccec04a1ed899cfeaf001a6800ebee
Andrija Jovanovic <andrijajovanovic001@gmail.com>
2024-03-05 10:41:29 +0100

    small tweaks on skills page

--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -3,31 +3,32 @@ import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import { meta, skill } from "../../content_option";
-import { MDBTabs, MDBTabsItem } from 'mdb-react-ui-kit';
 
 export const Skills = () => {
+  const containerHeight = `calc(100vh - 220px)`; // Calculate the height
+
   return (
     <HelmetProvider>
-      <div className="overflow-hidden" style={{ height: "100vh" }}>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Skills | {meta.title}</title>
-          <meta name="description" content={meta.description} />
-        </Helmet>
-        <Container className="About-header d-flex justify-content-center align-items-center mx-auto p-2 m-2 h-100">
-          <Row className="sec_sp">
-            <Col lg="7" className="d-flex justify-content-center align-items-center h-100 mx-auto flex-wrap text-center">
+      <Container fluid className="About-header d-flex justify-content-center align-items-center p-2 m-2" style={{ height: containerHeight }}>
+        <div className="overflow-hidden w-100">
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Skills | {meta.title}</title>
+            <meta name="description" content={meta.description} />
+          </Helmet>
+          <Row className="w-100">
+            <Col lg="7" className="d-flex mx-auto justify-content-center align-items-center flex-wrap text-center">
               {skill.map((data, i) => {
                 return (
-                  <div key={i} className=' mx-auto'>
-                    <h3 className="progress-title btn btn-static">{data.name}</h3>
+                  <div key={i} className='mx-auto'>
+                    <h3 className="btn btn-static">{data.name}</h3>
                   </div>
                 );
               })}
             </Col>
           </Row>
-        </Container>
-      </div>
+        </div>
+      </Container>
     </HelmetProvider>
   );
 };
